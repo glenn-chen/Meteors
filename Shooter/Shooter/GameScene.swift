47112c2cae3b78b11d6abcd2174b3b1cec3ac9b3
glenn-chen <glenn.jun.chen@gmail.com>
2017-07-05 16:18:01 -0700

    completed base game

--- Shooter/Shooter/GameScene.swift
+++ Shooter/Shooter/GameScene.swift
@@ -9,36 +9,124 @@
 import SpriteKit
 import GameplayKit
 
+func distance(_ from: CGPoint, _ to: CGPoint) -> Double {
+    let distanceSquared = (from.x - to.x) * (from.x - to.x) + (from.y - to.y) * (from.y - to.y)
+    return sqrt(Double(distanceSquared))
+}
+
 class GameScene: SKScene {
+    
+    enum GameState {
+        case active, gameOver
+    }
+    var gameState = GameState.active
     
     var meteorLayer: SKNode!
     var meteorSource: SKNode!
     var scoreLabel: SKLabelNode!
+    var restartButton: MSButtonNode!
+    var laser: SKNode!
     
+    var laserTimer: CFTimeInterval = 0 {
+        didSet {
+            if laserTimer > 0.3 {
+                laser.isHidden = true
+            }
+        }
+    }
     var spawnTimer: CFTimeInterval = 0
     let fixedDelta: CFTimeInterval = 1.0 / 50.0 /* 50 FPS */
-    var points: Int = 0
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "\(score)"
+        }
+    }
+    
+    let meteorRadius = 20.0
     
     override func didMove(to view: SKView) {
         /* Setup your scene here */
         
+        laser = self.childNode(withName: "laser")
+        laser.isHidden = true
+        
         scoreLabel = self.childNode(withName: "scoreLabel") as! SKLabelNode
         
-        /* Set reference to obstacle layer node */
+        restartButton = self.childNode(withName: "restartButton") as! MSButtonNode
+        restartButton.state = .MSButtonNodeStateHidden
+        /* Setup restart button selection handler */
+        restartButton.selectedHandler = {
+            
+            /* Grab reference to our SpriteKit view */
+            let skView = self.view as SKView!
+            
+            /* Load Game scene */
+            let scene = GameScene(fileNamed:"GameScene") as GameScene!
+            
+            /* Ensure correct aspect mode */
+            scene?.scaleMode = .aspectFill
+            
+            /* Restart game scene */
+            skView?.presentScene(scene)
+        }
+        
+        /* Set reference to meteor layer node */
         meteorLayer = self.childNode(withName: "meteorLayer")
         
         meteorSource = self.childNode(withName: "meteor")
         
-        scoreLabel.text = "\(points)"
+        scoreLabel.text = "\(score)"
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         /* Called when a touch begins */
+        if gameState == .gameOver {
+            return
+        }
+        
+        let touch = touches.first!
+        let touchLocation = touch.location(in: self)
+        
+        laserTimer = 0
+        laser.position.x = touchLocation.x
+        laser.isHidden = false
+        
+        for meteor in meteorLayer.children {
+            let meteorNode = meteor.children[0].children[0]
+            let meteorPosition = meteorNode.convert(meteorNode.position, to: self)
+            
+    /*        print("touch \(touchLocation)")
+            print(meteorPosition) */
+            
+            if abs(meteorPosition.x - touchLocation.x) <= CGFloat(meteorRadius) {
+                meteor.removeFromParent()
+                score += 1
+                
+                let particles = SKEmitterNode(fileNamed: "Explosion")!
+                /* Position particles at the Seal node
+                 If you've moved Seal to an sks, this will need to be
+                 node.convert(node.position, to: self), not node.position */
+                particles.position = CGPoint(x: touchLocation.x, y: meteorPosition.y + size.height - 100)
+                /* Add particles to scene */
+                addChild(particles)
+                let wait = SKAction.wait(forDuration: 0.7)
+                let removeParticles = SKAction.removeFromParent()
+                let seq = SKAction.sequence([wait, removeParticles])
+                particles.run(seq)
+            }
+        }
     }
     
     override func update(_ currentTime: TimeInterval) {
         /* Called before each frame is rendered */
+        
+        if gameState == .gameOver {
+            return
+        }
+        
         spawnTimer += fixedDelta
+        
+        laserTimer += fixedDelta
         
         updateMeteors()
     }
@@ -48,28 +136,28 @@ class GameScene: SKScene {
         for meteor in meteorLayer.children as! [SKReferenceNode] {
            // print(meteorLayer.children.count)
             /* Get obstacle node position, convert node position to scene space */
-            let meteorPosition = meteorLayer.convert(meteor.position, to: self)
-            
+            let meteorNode = meteor.children[0].children[0]
+            let realPosition = meteorNode.convert(meteorNode.position, to: self)
+          //  print(realPosition)
             /* Check if obstacle has left the scene */
-            if meteorPosition.y <= -20 {
-                // 20 is one half the width of an obstacle
-                print("yee")
-                /* Remove obstacle node from obstacle layer */
+            if realPosition.y <= -size.height {
+                /* Game over */
                 meteor.removeFromParent()
+                scoreLabel.text = "how disappointing"
+                laser.isHidden = true
+                gameState = .gameOver
+                restartButton.state = .MSButtonNodeStateActive
             }
-            
         }
         
         if spawnTimer >= 1.0 {
             /* Create a new meteor by copying the source meteor */
-            
             let newMeteor = meteorSource.copy() as! SKNode
-            meteorLayer.addChild(newMeteor)
             
             /* Generate new meteor position */
             let randomPosition = CGPoint(x: CGFloat.random(min: 20, max: 548), y: 350)
             newMeteor.position = self.convert(randomPosition, to: meteorLayer)
-            
+            meteorLayer.addChild(newMeteor)
             // Reset spawn timer
             spawnTimer = 0
         }
